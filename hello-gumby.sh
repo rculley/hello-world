c5436a922d9b890d863e9a6bca6811b3403194c0
Ryan Culley <rculley@ciena.com>
2016-08-09 16:46:22 -0700

    adding hello-gumby.sh

---
+++ hello-gumby.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "hello gumby"
+echo "clinton is crooked"
+
+
+# which bash > script_name.sh
+# vim script_name.sh
+# add "#!" before /bin/bash so looks like "#!/bin/bash"
+# add anything else
+# change the permission of file "chmod +x script_name.sh"
